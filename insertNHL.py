dea93852070bb0b2f1db4ba2a252b3c2e194ef31
Kwaku <kwakuappiahadu@gmail.com>
2019-10-25 13:49:10 +0000

    modified insertNHL function

--- insertNHL.py
+++ insertNHL.py
@@ -4,11 +4,11 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client['NHL']
-collection_currency = db['NHLdata']
+collection_nhl = db['NHLdata']
 
 with open('nhl.json') as f:
     file_data = json.load(f)
 
 # use collection_currency.insert(file_data) if pymongo version < 3.0
-collection_currency.insert_many(file_data)  
+collection_nhl.insert_many(file_data)  
 client.close()
